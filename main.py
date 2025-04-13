19b6f987739c19b3bd5ab9b437710470c87d44be
Jason Wang <JasonWang9073E@gmail.com>
2025-04-13 20:00:44 +0800

    main.py -> simple script

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class FlowquariumApp(App):
+    pass
+
+if __name__ == "__main__":
+    FlowquariumApp().run()
