2280e23c448797edb3272966c4bd30fa2ce2c0d7
Jason Wang <JasonWang9073E@gmail.com>
2025-05-11 13:24:55 +0800

    Moved KV code into Flowquarium.kv file.

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-from kivy.lang import Builder
 from kivymd.app import MDApp
-
-KV_FILE = """
-#:import colors kivymd.color_definitions.colors
-
-MDScreen:
-    md_bg_color: colors[app.theme_cls.primary_palette]["200"]
-    MDFillRoundFlatButton:
-        text: "Start Timer"
-        pos_hint: {"center_x": .5, "center_y": .3}
-        md_bg_color: app.theme_cls.primary_color
-"""
 
 
 class FlowquariumApp(MDApp):
-    def build(self):
+    def build(self) -> None:
         self.theme_cls.primary_palette = "Blue"
-        return Builder.load_string(KV_FILE)
 
 
 if __name__ == "__main__":
