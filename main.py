7addceb8ce1206aecebb28f0b2b0ae8a6d27ffe0
Jason Wang <JasonWang9073E@gmail.com>
2025-05-11 21:22:05 +0800

    made on_button_press method

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from kivymd.app import MDApp
+import timer
 
 
 class FlowquariumApp(MDApp):
     def build(self) -> None:
         self.theme_cls.primary_palette = "Blue"
+
+    def on_button_press(self):
+        pass
 
 
 if __name__ == "__main__":
